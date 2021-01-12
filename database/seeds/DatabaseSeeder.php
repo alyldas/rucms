7ffb92dcc5c94bbe12092d039a4ba3df5d0b6f2e
Andrey Markov <markov@workmail.com>
2021-01-13 00:09:45 +0300

    Installed jeremykenedy / laravel-roles
    Fixed 2 or 3 bugs during installation... :new_moon_with_face:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(ConnectRelationshipsSeeder::class);
+        //$this->call('UsersTableSeeder');
+
+        Model::reguard();
     }
 }
